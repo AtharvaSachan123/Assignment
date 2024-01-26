3e0d7bf5da67e5fb0dac20eea7aff7586999d62b
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-01-27 00:29:58 +0530

    Routing done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
-import Home from './components/Home';
+// Import necessary dependencies
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
+// Wrap your components with Router
 function App() {
   return (
-  <div >
-    <Navbar/>
-    <Home/>
-  </div>
+    <Router>
+       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      
+    </Router>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,67 +1,86 @@
-import React, { useState } from 'react'
-import {FaBars,FaTimes} from 'react-icons/fa'
-import {Link} from 'react-scroll'
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { useLottie } from 'lottie-react';
+import animation from '../Lottie/lottie.json';
+
 const Navbar = () => {
-const [nav,setNav]=useState(false);
-  const links =[
+  const options = {
+    animationData: animation,
+    loop: true,
+  };
+
+  const { View } = useLottie(options);
+
+  const [nav, setNav] = useState(false);
+
+  const links = [
     {
       id: 1,
-      link: "home"
+      link: 'home',
+      path: '/',
     },
     {
       id: 2,
-      link: "cart"
+      link: 'cart',
+      path: '/cart',
     },
     {
-      id: 3,
-      link: "store"
+        id: 3,
+        link: "store",
+        path: "/store"
+      },
+      {
+        id: 4,
+        link: "Login",
+        path: "/login"
+      },
+      {
+        id: 5,
+        link: "signup",
+        path:"/signup"
     },
-    {
-      id: 4,
-      link: "Login"
-    },
-    {
-      id: 5,
-      link: "signup"
-    },
+  ];
 
-  ]
   return (
-    <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black '>
-      <div>
-        <h1 className='text-5xl font-signature ml-2'>BookStore</h1>
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black ">
+      <div className="flex gap-5 ">
+        <h1 className="text-5xl font-signature ml-2 mt-4">BookStore</h1>
+        <div className="w-10 ">{View}</div>
       </div>
-     <ul className='hidden md:flex'>
-      {links.map(({id,link}) =>(
-              <li
-               key={id}
-               className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200 hover:text-blue-400'>
-                <Link to={link} smooth duration={500}>{link}</Link>
-               </li>
-
-      ))}
-     </ul>
-     <div onClick={()=>setNav(!nav)} className='cursor-pointer pr-4 z-10 text-gray-500 md:hidden'>
-      {nav ? <FaTimes size={30}/>: <FaBars size={30}/>}
-     </div>
+      <ul className="hidden md:flex">
+        {links.map(({ id, link, path }) => (
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200 hover:text-blue-400">
+            <Link to={path}>{link}</Link>
+          </li>
+        ))}
+      </ul>
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden">
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
       {nav && (
-         <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
-         {links.map(({id,link}) =>(
-                 <li
-                  key={id}
-                  className='px-4 cursor-pointer capitalize py-6 text-4xl'>
-                                   <Link onClick={()=>setNav(!nav)} to={link} smooth duration={500}>{link}</Link>
-
-                  </li>
-   
-         ))}        
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, link, path }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl">
+              <Link
+                onClick={() => setNav(!nav)}
+                to={path}
+                smooth
+                duration={500}>
+                {link}
+              </Link>
+            </li>
+          ))}
         </ul>
       )}
-      
-
-    
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
