9873ce5a8e10ab4634e5d5b282ec2dcb2fea5b23
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-01-27 22:01:20 +0530

    Done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,9 +18,9 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 gap-2 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            Welcome to the Store!!!
+            Welcome to the BookCraze!!!
           </h2>
-          <p className="text-gray-500 py-2 max-w-md">
+          <p className="text-gray-500 py-2 text-2xl  max-w-md">
             One-stop shop for every book you need!!!!
           </p>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black ">
       <div className="flex gap-5 ">
-        <h1 className="text-5xl font-signature ml-2 mt-4">BookStore</h1>
+        <h1 className="text-5xl font-signature ml-2 mt-4">BookCraze</h1>
         <div className="w-10 ">{View}</div>
       </div>
       <ul className="hidden md:flex">
